ac1a1144285fdad2b726c7bb139a7eb287cc5afe
nayuta <isato.3710@gmail.com>
2026-01-15 22:17:17 +0900

    Update homebrew formula to v1.0.2

--- Formula/macmic.rb
+++ Formula/macmic.rb
@@ -1,8 +1,8 @@
 class Macmic < Formula
   desc "The realtime mic monitor tool"
   homepage "https://github.com/haruaki-nayuta/MacMic"
-  url "https://github.com/haruaki-nayuta/MacMic/archive/refs/tags/v1.0.1.tar.gz"
-  sha256 "eb173f73a43469f2fc33a710527cd8719639e9010d7e63b575288c4b3d07fa52"
+  url "https://github.com/haruaki-nayuta/MacMic/archive/refs/tags/v1.0.2.tar.gz"
+  sha256 "fc483c486ac8f28970bc44c6e0dbf56cd825191a885257ff936164feef509d24"
   license "MIT"
 
   depends_on :xcode => ["12.0", :build]
